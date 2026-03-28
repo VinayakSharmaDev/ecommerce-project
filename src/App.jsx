d76cf8db92ed95bdf76b69109d84191dc1af6fee
VinayakSharmaDev <vinayaksharma.dev@gmail.com>
2026-03-28 04:53:47 -0700

    created multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-// import { useState } from 'react'
+import {Routes, Route, Navigate} from 'react-router';
 import { HomePage } from '/src/pages/HomePage';
 import './App.css'
 
 function App() {
-return(
-    <>
-    <HomePage></HomePage>
-    </>
-);
+    return (
+        <>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home"/>}> </Route>
+                    <Route path="/home" element={<HomePage/>}></Route>
+                    <Route path="/checkout" element={<div>hello world</div>}></Route>
+                </Routes>
+        </>
+    );
 };
 
 export default App;
