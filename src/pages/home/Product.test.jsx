43a365d2735f9dd90238ef62e6fdd3ebbaeb6630
VinayakSharmaDev <vinayaksharma.dev@gmail.com>
2026-04-16 04:43:42 -0700

    used beforeEach, in case data updates

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import '@testing-library/jest-dom';
@@ -7,20 +7,24 @@ import Product from './Product';
 
 vi.mock('axios');
 
-const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-};
+let product;
+let loadCart;
 
-vi.mock('axios');
-const loadCart = vi.fn();
+beforeEach(() => {
+    product = {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+            stars: 4.5,
+            count: 87
+        },
+        priceCents: 1090,
+        keywords: ["socks", "sports", "apparel"]
+    };
+
+    loadCart = vi.fn();
+})
 
 describe('Product Component', () => {
     it('display the product details correctly', () => {
