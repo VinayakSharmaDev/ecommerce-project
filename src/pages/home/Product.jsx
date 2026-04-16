ca5781db4dbe88dce5079d44798ff9644f6c1387
VinayakSharmaDev <vinayaksharma.dev@gmail.com>
2026-04-16 03:54:54 -0700

    test clicking add to card button

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -75,7 +75,7 @@ function Product({ product, loadCart }) {
                 Added
             </div>
 
-            <button className="add-to-cart-button button-primary" onClick={addToCart}>
+            <button className="add-to-cart-button button-primary" data-testid="add-to-cart" onClick={addToCart}>
                 Add to Cart
             </button>
         </div>
--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,7 +1,11 @@
+import axios from 'axios';
 import { it, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import Product from './Product';
 
+vi.mock('axios');
 
 const product = {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -15,6 +19,7 @@ const product = {
     keywords: ["socks", "sports", "apparel"]
 };
 
+vi.mock('axios');
 const loadCart = vi.fn();
 
 describe('Product Component', () => {
@@ -43,4 +48,21 @@ describe('Product Component', () => {
 
 
     });
+
+    it('check if it is interactive', async () => {
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+        const addToCartBtn = screen.getByTestId('add-to-cart');
+        await user.click(addToCartBtn);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        )
+        expect(loadCart).toHaveBeenCalled();
+    })
 });
